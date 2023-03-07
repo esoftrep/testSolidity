961ba2fdf7651c23a814d5af2c8bd6ed1c7546bf
bruno <125370015+esoftrep@users.noreply.github.com>
2023-03-07 10:56:43 +0100

    Create test1.sol
    exo4_visibility_20230307

---
+++ test1.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.4;
+
+contract Function {
+    
+    int public answerToLife = 42;
+    int public wrongAnswerToLife = -42;
+    string private sentence = "I'm a string";
+    bool private isTrue = true;
+    address private owner = 0x012F562E6A8623414121D547FD44fa36C14C9d18;
+    
+      }
